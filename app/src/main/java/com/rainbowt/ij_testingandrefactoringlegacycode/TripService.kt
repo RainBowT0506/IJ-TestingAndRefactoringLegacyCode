f495cb0441aad577e7444ec2209a0c0c85e269fe
RainBowT <rainbowt0506@gmail.com>
2024-02-25 03:00:17 +0800

    Hack - Refactor TripService method for clarity and consistency
    - Extract method validate() to handle user login validation, improving readability and abstraction level
    - Rename new array list creation to a more descriptive function call noTrips()
    - Update method signature to accept loggedInUser parameter for better clarity and domain relevance
    - Simplify method logic and language to enhance understandability and maintainability

--- app/src/main/java/com/rainbowt/ij_testingandrefactoringlegacycode/TripService.kt
+++ app/src/main/java/com/rainbowt/ij_testingandrefactoringlegacycode/TripService.kt
@@ -7,15 +7,22 @@ open class TripService(private val tripDAO: TripDAO) {
     @Throws(UserNotLoggedInException::class)
     fun getTripsByUser(user: User?, loggedInUser: User?): List<Trip> {
 
-        if (loggedInUser == null){
+        validate(loggedInUser)
+        
+        return if (user != null && user.isFriendWith(loggedInUser!!)) {
+            TripDAO.findTripsByUser(user);
+        } else {
+            noTrips()
+        }
+    }
+
+    private fun noTrips(): List<Trip> =
+        listOf()
+
+    private fun validate(loggedInUser: User?) {
+        if (loggedInUser == null) {
             throw UserNotLoggedInException()
         }
-
-        var trips: List<Trip> = listOf()
-        if (user != null && user.isFriendWith(loggedInUser)) {
-            trips = TripDAO.findTripsByUser(user);
-        }
-        return trips
     }
 
     open fun tripsBy(user: User): List<Trip> {
