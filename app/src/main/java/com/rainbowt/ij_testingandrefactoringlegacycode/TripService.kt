41a7828c99c9dd3b4fb9b631928912497ded890b
RainBowT <rainbowt0506@gmail.com>
2024-02-25 01:52:56 +0800

    For Loops & Delete Variables
    Refactor getTripsByUser method for improved readability and simplicity
    
    - Remove unnecessary variables and streamline logic to enhance code clarity
    - Move variable declarations closer to their usage points for better code organization
    - Simplify the friend-checking process using the 'isFriendWith' method in the User class
    - Ensure that exceptions are thrown appropriately for null user or logged-in user cases

--- app/src/main/java/com/rainbowt/ij_testingandrefactoringlegacycode/TripService.kt
+++ app/src/main/java/com/rainbowt/ij_testingandrefactoringlegacycode/TripService.kt
@@ -4,23 +4,18 @@ open class TripService {
 
     @Throws(UserNotLoggedInException::class)
     fun getTripsByUser(user: User?): List<Trip> {
-        var trips: List<Trip> = listOf()
-        val loggedUser = loggedInUser()
-        var isFriend = false
 
-        user?.let {
-            for (friend in user.friends) {
-                if (friend == loggedUser) {
-                    isFriend = true
-                    break
-                }
-            }
-            if (isFriend) {
+        val loggedUser = loggedInUser()
+
+        if (user != null && loggedUser != null) {
+            var trips: List<Trip> = listOf()
+            if (user.isFriendWith(loggedUser)) {
                 trips = TripDAO.findTripsByUser(user);
             }
-
             return trips
-        } ?: throw UserNotLoggedInException()
+        } else {
+            throw UserNotLoggedInException()
+        }
     }
 
     open fun tripsBy(user: User): List<Trip> {
