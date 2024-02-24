2e2106ba916e62bfa3b1073225de20be2b5a037b
RainBowT <rainbowt0506@gmail.com>
2024-02-24 22:14:21 +0800

    Enhance testing and storytelling in TripServiceTest
    - Introduce seam-based testing to enable better control over dependencies
    - Refactor test method 'validate_the_logged_in_user' to use predefined constants for clarity and storytelling
    - Add test method 'return_no_trips_when_users_are_not_friends' to verify behavior when users are not friends
    - Improve test coverage and readability by creating instances of registered users and strangers
    - Utilize AssertJ for more expressive assertions

--- app/src/main/java/com/rainbowt/ij_testingandrefactoringlegacycode/User.kt
+++ app/src/main/java/com/rainbowt/ij_testingandrefactoringlegacycode/User.kt
@@ -3,8 +3,13 @@ package com.rainbowt.ij_testingandrefactoringlegacycode
 class User {
 
     private var friends: List<User> = listOf()
-
-    fun getFriends(): List<User> {
-        return friends
-    }
+        get() = field
+        set(value) {
+            field = value
+        }
+    private var trips: List<Trip> = listOf()
+        get() = field
+        set(value) {
+            field = value
+        }
 }
--- app/src/test/java/com/rainbowt/ij_testingandrefactoringlegacycode/TripServiceTest.kt
+++ app/src/test/java/com/rainbowt/ij_testingandrefactoringlegacycode/TripServiceTest.kt
@@ -1,20 +1,46 @@
 package com.rainbowt.ij_testingandrefactoringlegacycode
 
+
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
+
 class TripServiceTest {
+    private var _loggedInUser: User? = null
 
     @Test(expected = UserNotLoggedInException::class)
     fun validate_the_logged_in_user() {
         val tripService = TestableTripService()
 
-        tripService.getTripsByUser(null)
+        _loggedInUser = Companion.GUEST
+
+
+        tripService.getTripsByUser(ANY_USER)
     }
 
-    private class TestableTripService : TripService(){
+    @Test(expected = UserNotLoggedInException::class)
+    fun return_no_trips_when_users_are_not_friends() {
+        val tripService = TestableTripService()
+
+        _loggedInUser = REGISTERED_USER
+
+        val stranger = User()
+        val trips = tripService.getTripsByUser(stranger)
+
+        assertThat(trips).isEmpty()
+
+    }
+
+    private inner class TestableTripService : TripService() {
 
         override fun loggedInUser(): User? {
-            return null
+            return _loggedInUser
         }
+    }
+
+    companion object {
+        private val GUEST = null
+        private val ANY_USER = User()
+        private val REGISTERED_USER = User()
     }
 }
